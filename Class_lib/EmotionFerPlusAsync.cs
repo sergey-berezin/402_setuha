9370118a20a4a1b68c5096ab0a8c1259cd31c8a2
Алёна Сетуха <setyhaalena29@gmail.com>
2022-10-05 17:23:32 +0300

    3 commit

---
+++ Class_lib/EmotionFerPlusAsync.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using SixLabors.ImageSharp; // Из одноимённого пакета NuGet
+using SixLabors.ImageSharp.PixelFormats;
+using System.Threading;
+using System.Linq;
+using SixLabors.ImageSharp.Processing;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Collections.Specialized.BitVector32;
+using System.Threading.Tasks;
+
+
+namespace Lib1
+{
+    public class EmotionFerPlusAsync: IDisposable   
+    {
+        private InferenceSession _session;
+        private bool _session_dispose = false;
+
+//============================Конструктор=======================================
+        public EmotionFerPlusAsync()
+        {
+            using var modelStream = typeof(EmotionFerPlusAsync).Assembly
+                .GetManifestResourceStream("Lib1.emotion-ferplus-7.onnx");
+            using var memoryStream = new MemoryStream();
+            modelStream.CopyTo(memoryStream);
+            this._session = new InferenceSession(memoryStream.ToArray());
+        }
+
+
+//===============================Функция распознавания эмоций====================
+        public async Task<List<Tuple<string, float>>> Recognition_func
+                (DenseTensor<float> input, CancellationToken token)
+        {
+            //Проверка на удаление сессии(чистка памяти)
+            if (_session_dispose)
+            {
+                throw new ObjectDisposedException("session is disposed");
+            }
+            //Проверка на внешнее прерывание
+            token.ThrowIfCancellationRequested();
+            return await Task<List<Tuple<string, float>>>.Factory.StartNew(() =>
+            {
+                //Проверка на внешнее прерывание
+                token.ThrowIfCancellationRequested();
+                IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results;
+                //Предобработка тензора
+                var inputs = new List<NamedOnnxValue>
+                {
+                    NamedOnnxValue.CreateFromTensor("Input3", input)
+                };
+                //Запуск сессии
+                lock (_session)
+                {
+                    results = _session.Run(inputs);
+                }
+                //Формирование результата
+                var emotions = Softmax(results.First(v =>
+                            v.Name == "Plus692_Output_0").AsEnumerable<float>().ToArray());
+
+                List<Tuple<string, float>> result = new();
+                string[] keys = { "neutral", "happiness", "surprise", "sadness",
+                        "anger", "disgust", "fear", "contempt" };
+                foreach (var i in keys.Zip(emotions))
+                {
+                    result.Add(Tuple.Create(i.First, i.Second));
+                }
+                return result;
+
+            }, token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+
+
+        public void Dispose()
+        {
+            _session.Dispose();
+            _session_dispose = true;
+        }
+
+
+//=======================Обычный несинхронный метод==============================
+        public static float[] Softmax(float[] z)
+        {
+            var exps = z.Select(x => Math.Exp(x)).ToArray();
+            var sum = exps.Sum();
+            return exps.Select(x => (float)(x / sum)).ToArray();
+        }
+    }
+}
---
+++ Console_App/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using SixLabors.ImageSharp; // Из одноимённого пакета NuGet
+using SixLabors.ImageSharp.PixelFormats;
+using System.Linq;
+using SixLabors.ImageSharp.Processing;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
+using System.Collections.Generic;
+using System.IO;
+using Lib1;
+
+
+
+//Загрузка картинки
+using Image<Rgb24> image = Image.Load<Rgb24>("face1.png");
+image.Mutate(ctx => {
+    ctx.Resize(new Size(64, 64));
+    // ctx.Grayscale();
+});
+
+//Перевод картинки в тензор
+DenseTensor<float> image_tensor = GrayscaleImageToTensor(image);
+
+
+using EmotionFerPlusAsync emotionFerPlus = new();
+var r = await emotionFerPlus.Recognition_func(image_tensor, CancellationToken.None);
+
+foreach (var item in r)
+{
+    Console.WriteLine(item.Item1 + " " + item.Item2.ToString());
+}
+
+
+
+
+DenseTensor<float> GrayscaleImageToTensor(Image<Rgb24> img)
+{
+    var w = img.Width;
+    var h = img.Height;
+    var t = new DenseTensor<float>(new[] { 1, 1, h, w });
+
+    img.ProcessPixelRows(pa =>
+    {
+        for (int y = 0; y < h; y++)
+        {
+            Span<Rgb24> pixelSpan = pa.GetRowSpan(y);
+            for (int x = 0; x < w; x++)
+            {
+                t[0, 0, y, x] = pixelSpan[x].R; // B and G are the same
+            }
+        }
+    });
+
+    return t;
+}
