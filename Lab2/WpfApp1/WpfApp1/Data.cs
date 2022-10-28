95d9d53b37a64d8b0b3de4882de0548579993308
Алёна Сетуха <setyhaalena29@gmail.com>
2022-10-28 19:32:25 +0300

    Lab2

---
+++ Lab2/WpfApp1/WpfApp1/Data.cs
@@ -0,0 +1,44 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1
+{
+    public class Data: INotifyPropertyChanged
+    {
+        private string _image_name;
+        public string image_name
+        {
+            get => _image_name;
+            set
+            {
+                _image_name = value;
+                OnPropertyChanged("ImageName");
+            }
+        }
+
+        private List<Tuple<string, float>> _emotions;
+        public List<Tuple<string, float>> emotions { 
+            get => _emotions; 
+            set
+            {
+                _emotions = value;
+                OnPropertyChanged("Emotions");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
